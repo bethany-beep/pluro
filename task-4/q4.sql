6e72eda224043a1578e7eefe291baaa91f3c184a
Bethany Talbot <bethanytalbot@MacBook-Air-6.local>
2020-05-12 18:03:29 +0100

    updating q4 comment

--- task-4/q4.sql
+++ task-4/q4.sql
@@ -1,3 +1,5 @@
 SELECT id, first_name, last_name
 FROM public.api_users
 WHERE first_name = 'Bethany' AND last_name = 'Talbot';
+
+#you wouldn't not know information about the person you're searching for, exactly the same why you'd search in Admin
